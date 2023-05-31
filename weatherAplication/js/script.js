66d6cbaec9cc69aacee854c73a75b2747ff02a05
RcRichard <114629099+RcRichard@users.noreply.github.com>
2023-05-31 09:21:22 -0300

    updateProjects

---
+++ weatherAplication/js/script.js
@@ -0,0 +1,32 @@
+const cityForm = document.querySelector('[data-js="insert-location"]')
+
+const cityNameContainer = document.querySelector('[data-js="city-name"]')
+const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
+const citytemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+const cityCard = document.querySelector('[ data-js="city-card"]')
+let timeImg = document.querySelector('[data-js="time"]')
+
+
+cityForm.addEventListener('submit', async event => {
+    event.preventDefault()
+    const inputValue = event.target.city.value
+    const [{ Key, LocalizedName }] = await getCityData(inputValue)
+    const [{ WeatherText, Temperature, IsDayTime }] = await getCityWeather(Key)
+
+    if(cityCard.classList.contains('d-none')){
+        cityCard.classList.remove('d-none')
+    }
+
+    if(IsDayTime){
+        timeImg.src = './src/day.svg'
+    } else {
+        timeImg.src = './src/night.svg'
+    }
+     
+
+    cityNameContainer.textContent = LocalizedName
+    cityWeatherContainer.textContent = WeatherText
+    citytemperatureContainer.textContent = Temperature.Metric.Value
+
+    cityForm.reset()
+})
---
+++ weatherAplication/js/weather.js
@@ -0,0 +1,26 @@
+const apiKey = 'Gtg7eEG34cjNAeAXchhlUJDoCRceEJew'
+const baseUrl = `http://dataservice.accuweather.com/`
+
+const getCityUrl = cityName => `${baseUrl}locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
+
+const getWeatherUrl = cityKey => `${baseUrl}currentconditions/v1/${cityKey}?apikey=${apiKey}&language=pt-br`
+
+
+const fetchData = async url => {
+    try {
+
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error('Não foi possivel obter os dados ')
+        }
+
+        return await response.json()
+
+    } catch ({ name, message }) {
+        alert(`${name} : ${message}`)
+    }
+}
+
+const getCityData = cityName => fetchData(getCityUrl(cityName))
+
+const getCityWeather = cityKey => fetchData(getWeatherUrl(cityKey))
